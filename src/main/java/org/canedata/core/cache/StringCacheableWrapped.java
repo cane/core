464e8b30217eb2f51f5282679e6c0edf70a414d0
Sun Yat-ton <imsunyitao@gmail.com>
2014-03-31 14:12:46 +0800

    add cache wrapper

--- src/main/java/org/canedata/core/cache/StringCacheableWrapped.java
+++ src/main/java/org/canedata/core/cache/StringCacheableWrapped.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.canedata.core.util;
+package org.canedata.core.cache;
 
 import org.canedata.cache.Cacheable;
 
